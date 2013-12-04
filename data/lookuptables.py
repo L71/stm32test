b14d30c25114a6c9c2e63acd539d6f5b91985e16
Lars M Bengtsson <lars.m.bengtsson@gmail.com>
2013-12-04 15:52:28 +0100

    CLeaned up code formatting a bit + added sane defaults for prettyprinting function.

--- data/lookuptables.py
+++ data/lookuptables.py
@@ -6,18 +6,23 @@ import numpy
 def print_c_list(datatype, varname, data) :
 
     datasize=len(data)
-    cols = 8 # insert line break every N lines in data output
-    c = 0
+    # defaults print parameters...
+    cols=8 # insert line break every N lines in data output
+    formatstr='{:#9x}' # default printout format 8 pos Hex.
+    c=0
+    
+    # setup for specific data types
     if datatype == 'uint32_t' or datatype == 'int32_t' :
         formatstr='{:#11x}'
-        cols = 6
+        cols=6
     elif datatype == 'uint16_t' or datatype == 'int16_t' :
         formatstr='{:#7x}'
-        cols = 8
+        cols=8
     elif datatype == 'uint8_t' or datatype == 'int8_t' :
         formatstr='{:#5x}'
-        cols = 12
+        cols=12
     
+    # print C definition:
     print datatype, varname+'['+str(datasize)+']'+' = {'
     for i, e in enumerate(data) :
         # print e,
@@ -30,20 +35,17 @@ def print_c_list(datatype, varname, data) :
             c=0
     print '};'
     
-
-
-
 # This part calculates the phase stepping data for the phase accumulators
 # Output: list of phase counter values (uint32_t[]).
 # Equal 12-tone temperament
 
 def phasetable() :
-    keys = 12
-    fractions = 64   # steps in between each half note
-    base_freq = 2093   # frequency of C7, base note of the highest supported octave
-    base_key = 96   # MIDI key
+    keys=12
+    fractions=64   # steps in between each half note
+    base_freq=2093   # frequency of C7, base note of the highest supported octave
+    base_key=96   # MIDI key
 
-    pcm_freq = 40178.0    # PCM playback sample rate
+    pcm_freq=40178.0    # PCM playback sample rate
     pcm_step_max=2**32 # 32 bit phase counters
 
     i=0             # counter
@@ -51,8 +53,8 @@ def phasetable() :
     datalist=[]
 
     while i <= ( keys*fractions ) :
-        freq = base_freq*(2.0**(1.0/(keys*fractions)))**i
-        step = int(round(pcm_step_max/(pcm_freq/freq)))
+        freq=base_freq*(2.0**(1.0/(keys*fractions)))**i
+        step=int(round(pcm_step_max/(pcm_freq/freq)))
         # print i, freq, step
         datalist.append(step)
         i=i+1
@@ -64,13 +66,13 @@ def phasetable() :
 # 1024 samples * 12 bit resolution
 
 def sinewave() :
-    samples = 1024.0
-    amplitude = 2047.0    # P-P /2
+    samples=1024.0
+    amplitude=2047.0    # P-P /2
 
     i=0
     datalist=[]
     while i < samples :
-        value = numpy.sin((2*numpy.pi)*(i/samples))*amplitude 
+        value=numpy.sin((2*numpy.pi)*(i/samples))*amplitude 
         datalist.append(int(round(value)))
         i=i+1
     # print datalist
